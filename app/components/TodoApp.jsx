2af86e021a5de4d4f5aecdd0a2f8fa0352caea3f
catfriend <eakari@live.com>
2016-11-07 11:11:15 -0800

    add static todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,32 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Feed the cat'
+        }, {
+          id: 2,
+          text: 'Clean the house'
+        }, {
+          id: 3,
+          text: 'Get a job'
+        }, {
+          id: 4,
+          text: 'Win the lotto'
+        }
+      ]
+    };
+  },
   render: function (){
+    var {todos} = this.state; // pull state
+
     return (
       <div>
-        <h1>TodoApp.jsx</h1>
+        <TodoList todos={todos}/>
       </div>
     )
   }
